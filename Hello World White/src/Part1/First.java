71a5f725600ae3963526a1814bdee1c3f73d9811
megamupik <megamupik@yandex.ru>
2018-10-05 10:46:26 +0300

    Hello World

--- Hello World White/src/Part1/First.java
+++ Hello World White/src/Part1/First.java
@@ -2,6 +2,7 @@ package Part1;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.println("World");
     }
 }
