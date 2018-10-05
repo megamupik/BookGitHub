1a252fef7fa34686b271f1ab516848114352024a
megamupik <megamupik@yandex.ru>
2018-10-05 13:38:14 +0300

    !

--- Hello World White/src/Part1/First.java
+++ Hello World White/src/Part1/First.java
@@ -4,5 +4,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("World");
+        System.out.println("!!!");
     }
 }
